2a3e4df0a8f050d788610416dd4cbbf05c61e730
Diaz Blas <diazblasce@gmail.com>
2023-01-25 16:05:10 -0300

    back to html standar

--- force-app/main/default/lwc/criteriaCreateTable/criteriaCreateTable.js
+++ force-app/main/default/lwc/criteriaCreateTable/criteriaCreateTable.js
@@ -19,7 +19,7 @@ export default class CriteriaCreateTable extends LightningElement {
     @track wiredAccFieldsAndLabels = [];
     aux = [];
     //@track isDisable = false;
-    operationOptions = [];
+    @track operationOptions = [];
 
 /*     get operationOptions(){
         return [
@@ -134,6 +134,8 @@ export default class CriteriaCreateTable extends LightningElement {
             data.forEach(element => {
                 this.operationOptions.push({ label: `${element}`, value: `${element}`});
             });
+            console.log('HOLA PIANOLA1...', data);
+            console.log('HOLA PIANOLA2', this.operationOptions);
         }
     }
 
